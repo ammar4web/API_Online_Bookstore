33f0851ded3ebf85384046d0771807d88a30123a
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-07-27 21:49:16 +0300

    Link models to relationships

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo('App\Models\Publisher');
+    }
+
+    public function authors()
+    {
+        return $this->belongsToMany('App\Models\Author', 'book_author');
+    }
 }
