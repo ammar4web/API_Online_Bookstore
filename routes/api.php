654f767c4cc51650a8bffd616a3d81076e955eef
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-07-28 10:52:48 +0300

    Separate public routes into a separate file (the routes which doesn't require a login)

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\V1\AuthorController;
-use App\Http\Controllers\Api\V1\BookController;
-use App\Http\Controllers\Api\V1\CategoryController;
-use App\Http\Controllers\Api\V1\GalleryController;
-use App\Http\Controllers\Api\V1\PublisherController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,21 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
-Route::get('/search', [GalleryController::class, 'search'])->name('search');
-
-Route::get('/book/{book}', [BookController::class, 'details'])->name('book.details');
-
-Route::get('/categories', [CategoryController::class, 'list'])->name('gallery.categories.index');
-Route::get('/categories/search', [CategoryController::class, 'search'])->name('gallery.categories.search');
-Route::get('/categories/{category}', [CategoryController::class, 'result'])->name('gallery.categories.show');
-
-Route::get('/publishers', [PublisherController::class, 'list'])->name('gallery.publishers.index');
-Route::get('/publishers/search', [PublisherController::class, 'search'])->name('gallery.publishers.search');
-Route::get('/publishers/{publisher}', [PublisherController::class, 'result'])->name('gallery.publishers.show');
-
-Route::get('/authors', [AuthorController::class, 'list'])->name('gallery.authors.index');
-Route::get('/authors/search', [AuthorController::class, 'search'])->name('gallery.authors.search');
-Route::get('/authors/{author}', [AuthorController::class, 'result'])->name('gallery.authors.show');
-
+require __DIR__.'/public.php';
 require __DIR__.'/auth/auth.php';
---
+++ routes/public.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AuthorController;
+use App\Http\Controllers\Api\V1\BookController;
+use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\GalleryController;
+use App\Http\Controllers\Api\V1\PublisherController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
+Route::get('/search', [GalleryController::class, 'search'])->name('search');
+
+Route::get('/book/{book}', [BookController::class, 'details'])->name('book.details');
+
+Route::get('/categories', [CategoryController::class, 'list'])->name('gallery.categories.index');
+Route::get('/categories/search', [CategoryController::class, 'search'])->name('gallery.categories.search');
+Route::get('/categories/{category}', [CategoryController::class, 'result'])->name('gallery.categories.show');
+
+Route::get('/publishers', [PublisherController::class, 'list'])->name('gallery.publishers.index');
+Route::get('/publishers/search', [PublisherController::class, 'search'])->name('gallery.publishers.search');
+Route::get('/publishers/{publisher}', [PublisherController::class, 'result'])->name('gallery.publishers.show');
+
+Route::get('/authors', [AuthorController::class, 'list'])->name('gallery.authors.index');
+Route::get('/authors/search', [AuthorController::class, 'search'])->name('gallery.authors.search');
+Route::get('/authors/{author}', [AuthorController::class, 'result'])->name('gallery.authors.show');
