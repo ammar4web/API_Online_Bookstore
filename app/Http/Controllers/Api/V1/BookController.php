7de824d58d90d859a0a1fc3ed55ebd4cd22986b4
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-07-29 18:33:45 +0300

    Implemented API endpoints to view all books and to view a single book (for admin users in the control panel): Remove unnecessary methods from the book controller.

--- app/Http/Controllers/Api/V1/BookController.php
+++ app/Http/Controllers/Api/V1/BookController.php
@@ -14,15 +14,8 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $books = Book::all()->load(['publisher', 'category', 'authors']);
+        return $books;
     }
 
     /**
@@ -38,16 +31,9 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        //
+        return $book->load(['publisher', 'category', 'authors']);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Book $book)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Api\V1\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/admin')->middleware(['auth:sanctum', 'can:update-books'])->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+    Route::apiResource('/books', BookController::class);
 });
