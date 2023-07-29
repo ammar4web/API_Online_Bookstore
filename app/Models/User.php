062130f2cb5bdf13edf0cfcc844e5e121f6ec193
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-07-29 18:05:54 +0300

    Authorization To access the control panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function isAdmin()
+    {
+        return $this->administration_level > 0 ? true : false;
+    }
+
+    public function isSuperAdmin()
+    {
+        return $this->administration_level > 1 ? true : false;
+    }
 }
--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/admin')->group(function() {
+Route::prefix('/admin')->middleware(['auth:sanctum', 'can:update-books'])->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
 });
